9b4f96ab217b596034863ccb85b3eb5424d29bc2
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-12-15 01:03:27 -0500

    fix: remove unused main importations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { AuthProvider } from './context/AuthContext.tsx'
